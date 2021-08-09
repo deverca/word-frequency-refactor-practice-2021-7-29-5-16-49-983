88217ed63a70a8757b62cbfe19c0604be45df7a0
deverca <deverca@oocl.com>
2021-08-09 15:43:51 +0800

    [WORDFREQUENCY][CARMELA]refactor: optimized calculateWordFrequency logic

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -1,30 +1,24 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.StringJoiner;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class WordFrequencyGame {
-    public static final String WHITE_SPACE = "\\s";
+    public static final String WHITE_SPACE = "\\s+";
 
-    public String getResult(String sentence){
+    public String getResult(String sentence) {
 
 
-        if (sentence.split(WHITE_SPACE + "+").length==1) {
+        if (sentence.split(WHITE_SPACE).length == 1) {
             return sentence + " 1";
         } else {
 
             try {
-
-                List<WordCount> list = calculateWordFrequency(sentence);
-                List<WordCount> wordCountList;
-                wordCountList = list;
+                List<WordCount> wordCountList = calculateWordFrequency(sentence);
 
                 wordCountList.sort((w1, w2) -> w2.getWordCount() - w1.getWordCount());
 
                 StringJoiner joiner = new StringJoiner("\n");
                 for (WordCount w : wordCountList) {
-                    String s = w.getValue() + " " +w.getWordCount();
+                    String s = w.getValue() + " " + w.getWordCount();
                     joiner.add(s);
                 }
                 return joiner.toString();
@@ -37,46 +31,16 @@ public class WordFrequencyGame {
     }
 
     private List<WordCount> calculateWordFrequency(String sentence) {
-        String[] arr = sentence.split(WHITE_SPACE + "+");
 
-        List<WordCount> wordCountList = new ArrayList<>();
-        for (String s : arr) {
-            WordCount wordCount = new WordCount(s, 1);
-            wordCountList.add(wordCount);
-        }
+        List<String> words = Arrays.asList(sentence.split(WHITE_SPACE));
+        List<String> uniqueWords = words.stream().distinct().collect(Collectors.toList());
 
-        //get the map for the next step of sizing the same word
-        Map<String, List<WordCount>> map = getListMap(wordCountList);
-
-        List<WordCount> list = new ArrayList<>();
-        for (Map.Entry<String, List<WordCount>> entry : map.entrySet()) {
-            WordCount wordCount = new WordCount(entry.getKey(), entry.getValue().size());
-            list.add(wordCount);
-        }
-        return list;
+        List<WordCount> wordCounts = new ArrayList<>();
+        uniqueWords.forEach(uniqueWord -> {
+            int count = (int) words.stream().filter(uniqueWord::equals).count();
+            wordCounts.add(new WordCount(uniqueWord, count));
+        });
+        return wordCounts;
     }
-
-
-
-
-    private Map<String,List<WordCount>> getListMap(List<WordCount> wordCountList) {
-        Map<String, List<WordCount>> map = new HashMap<>();
-        for (WordCount wordCount : wordCountList){
-//       map.computeIfAbsent(input.getValue(), k -> new ArrayList<>()).add(input);
-            if (!map.containsKey(wordCount.getValue())){
-                ArrayList arr = new ArrayList<>();
-                arr.add(wordCount);
-                map.put(wordCount.getValue(), arr);
-            }
-
-            else {
-                map.get(wordCount.getValue()).add(wordCount);
-            }
-        }
-
-
-        return map;
-    }
-
 
 }
